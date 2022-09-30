dd12117c576a5381f28f138e5b215d5202c70bcc
kebjeb9616 <kebjeb91@gmail.com>
2022-09-30 17:05:46 +0700

    .

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+function navbar() {
+    return (
+        <>
+        <ul>
+            <li><a href="../app.js">opsi 1</a></li>
+            <li><a href="../app.js">opsi 2</a></li>
+            <li><a href="../app.js">opsi 3</a></li>
+        </ul>
+        </>
+    );
+}
+
+export default navbar;
